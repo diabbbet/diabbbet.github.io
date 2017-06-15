ea54a510bd694a299b28ae9273fe137cbf17d137
diabbbet <diabbbet@gmail.com>
2017-06-15 11:50:48 +0400

    bali

---
+++ bali/js/common.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	$('.carousel').slick({
+		slidesToShow: 3,
+		appendArrows: $('.carousel-buttons'),
+		prevArrow: '<button class="carousel-btn left"><img src="img/left-arrow.png" alt="arrow"></button>',
+		nextArrow: '<button class="carousel-btn right"><img src="img/right-arrow.png" alt="arrow"></button>',
+		responsive: [
+		{
+			breakpoint: 1800,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+
+});
