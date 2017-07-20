0b9f4c727c4bddbfe4214b4919cd714488726b3c
Nail Hamitov <diabbbet@gmail.com>
2017-07-20 17:40:34 +0400

    хотелка
    правки галерея телефон

--- bali/js/common.js
+++ bali/js/common.js
@@ -1,19 +1,13 @@
 $(function() {
 
 	$('.carousel').slick({
-		slidesToShow: 3,
+		slidesToShow: 2,
 		appendArrows: $('.carousel-buttons'),
 		prevArrow: '<button class="carousel-btn left"><img src="img/left-arrow.png" alt="arrow"></button>',
 		nextArrow: '<button class="carousel-btn right"><img src="img/right-arrow.png" alt="arrow"></button>',
 		responsive: [
 		{
-			breakpoint: 1800,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 1200,
+			breakpoint: 1400,
 			settings: {
 				slidesToShow: 1
 			}
