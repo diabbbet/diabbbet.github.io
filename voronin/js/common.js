02476b2cd1d70ce0e8f91de2ba54261df82c7719
Nail Hamitov <diabbbet@gmail.com>
2017-07-25 19:10:10 +0400

    код ревью

--- voronin/js/common.js
+++ voronin/js/common.js
@@ -1,5 +1,25 @@
 $(function() {
 
-	// Custom JS
+	$('.header').hover(function() {
+		$('.header').toggleClass('active');
+	});
+
+	$('.help-btn').click(function() {
+		$('section').removeClass('active');
+		$('.help').addClass('active');
+	});
+
+	$('.founder-btn').click(function() {
+		$('section').removeClass('active');
+		$('.founder').addClass('active');
+	});
+
+	$('.calendar-btn').click(function() {
+		$('section').removeClass('active');
+		$('.calendar').addClass('active');
+	});
+
+	$('.schedule-months').slick({
+	});
 
 });
