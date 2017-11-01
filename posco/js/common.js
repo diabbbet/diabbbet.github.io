4abcce66b10a4f0d360191aa240dd7b1baa60c90
Наиль Хамитов <diabbbet@gmail.com>
2017-11-01 10:39:39 +0400

    posco

--- posco/js/common.js
+++ posco/js/common.js
@@ -15,6 +15,17 @@ $(function() {
 	// pick-room
 	pickRoom();
 
+	// about-slider
+	if ($('.about-slider').length) {
+		aboutSlider();
+	}
+	
+
+	// location-map
+	if ($('#location-map').length) {
+		locationMap();
+	}
+
 	// contacts-map
 	if ($('#contacts-map').length) {
 		contactsMap();
@@ -56,9 +67,11 @@ $(function() {
 		$('.social-share').fadeToggle('fast');
 	});
 
-	$('.about-slider').slick({
-
-	});
+	// $('.logo').click(function(timeout) {
+	// 	event.preventDefault();
+	// 	$('.item-inner').animate({'opacity':'0'}, 500);
+	// 	return true;
+	// });
 
 });
 
@@ -68,7 +81,7 @@ $(function() {
 // content-height
 function contentHeight() {
 	var $contentHeight = $(window).height() - $('header').height() - $('.controllers').height();
-	$('.switcher, .selector').height($contentHeight);
+	$('.location').height($contentHeight);
 }
 
 
@@ -191,4 +204,227 @@ function contactsMap() {
 	marker.addListener('click', function() {
 		infowindow.open(map, marker);
 	});
+}
+
+
+// loaction-map
+function locationMap() {
+	
+	var coords = $("#location-map").data("coords").split(",");
+
+
+	var map = new google.maps.Map(document.getElementById('location-map'), {
+		zoom: 16,
+		center: new google.maps.LatLng(coords[0], coords[1]),
+		styles: [
+		{
+			"featureType": "all",
+			"elementType": "labels",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "labels.text",
+			"stylers": [
+			{
+				"visibility": "on"
+			}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "labels.text",
+			"stylers": [
+			{
+				"visibility": "on"
+			}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "all",
+			"stylers": [
+			{
+				"visibility": "on"
+			},
+			{
+				"lightness": "3"
+			}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "labels",
+			"stylers": [
+			{
+				"visibility": "on"
+			}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "labels.text.stroke",
+			"stylers": [
+			{
+				"visibility": "on"
+			}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "labels.icon",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+			{
+				"visibility": "on"
+			}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "labels.text",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		}
+		]
+	});
+
+	var infowindow = new google.maps.InfoWindow({
+		content: '<div id="feature-name"></div>'
+	});
+
+	var infowindow2 = new google.maps.InfoWindow({
+		content: '<div id="feature-img"></div>',
+		position: {lat: 53.222825, lng: 50.194407}
+	});
+
+	$('#locations-features span').each(function() {
+		var locationsCoords = $(this).data("coords").split(",");
+		var locationstype = $(this).data("type");
+		var locationstitle = $(this).data("title");
+		var iconBase = '../img/baloon_'+locationstype+'.png';
+
+		var locationObjectsIcon = new google.maps.MarkerImage(
+			"/img/baloon_"+locationstype+".png",
+			new google.maps.Size(40, 48)
+			);
+
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locationsCoords[0], locationsCoords[1]),
+			map: map,
+			title: locationstitle,
+			icon: locationObjectsIcon
+		});
+
+		marker.addListener('click', function() {
+			infowindow.open(map, marker);
+			$('#feature-name').html(locationstitle);
+		});
+	});
+
+	var coords_lb = $("#location-homes").data("coords-lb").split(",");
+	var coords_rt = $("#location-homes").data("coords-rt").split(",");
+	var img_maps = $("#location-homes").data("img-maps");
+	var data_img = $("#location-homes").data("img");
+	var imageBounds = new google.maps.LatLngBounds(
+		new google.maps.LatLng(coords_lb[0], coords_lb[1]), 
+		new google.maps.LatLng(coords_rt[0], coords_rt[1])
+		);
+
+	var mapimg = new google.maps.GroundOverlay(img_maps,imageBounds);
+	mapimg.setMap(map);
+
+	mapimg.addListener('click', function() {
+		infowindow2.open(map, mapimg);
+		$('#feature-img').html('<img src='+data_img+'>');
+	});
+
+
+}
+
+// about-slider
+function aboutSlider() {
+
+	$('.about-slider').on('init', function(){
+		var countSlides = $('.about-slider--item.slick-active').index();
+		$('.about-counter .count').html(countSlides);
+	});
+
+	$('.about-slider').slick({
+		infinite: true,
+		prevArrow: '<button class="prev"><i class="icon icon-back"></i></button>',
+		nextArrow: '<button class="next"><i class="icon icon-next"></i></button>'
+	});
+
+	var quantitySlides = $('.about-slider--item:not(.slick-cloned)').length;
+	$('.about-counter .quantity').html(quantitySlides);
+
+	$('.about-slider').on('afterChange', function(){
+		var countSlides = $('.about-slider--item.slick-active').index();
+		$('.about-counter .count').html(countSlides);
+	});
+
+	$('.modal-gallery .exit').on('click', function() {
+		$('.modal-gallery').fadeOut('fast');
+		$('.modal-slider').slick('unslick');
+		$('.modal-slider').html('');
+	});
+
+	$('.about-fullscreen').on('click', function() {
+		$('.modal-gallery').fadeIn('fast');
+		$('.about-slider--item:not(.slick-cloned)').each(function() {
+			var dataImg = $(this).attr('data-img');
+			var dataTitle = $(this).attr('data-title');
+			$('.modal-slider').append('<div class="modal-slider--item" style="background-image: url('+dataImg+')" data-title="'+dataTitle+'"></div>');
+		});
+
+		modalSlider();
+	});
+
+	function modalSlider() {
+
+		$('.modal-slider').on('init', function(){
+
+			var modalTitle = $('.modal-slider--item.slick-active').attr('data-title');
+			var countSlidesModal = $('.modal-slider--item.slick-active').index();
+
+			$('.modal-counter .count').html(countSlidesModal);
+			$('.modal-title').html(modalTitle);
+
+		});
+
+		$('.modal-slider').slick({
+			prevArrow: '<button class="prev"><i class="icon icon-button-arrow"></i></button>',
+			nextArrow: '<button class="next"><i class="icon icon-button-arrow"></i></button>'
+		});
+
+		var quantitySlidesModal = $('.modal-slider--item:not(.slick-cloned)').length;
+		$('.modal-counter .quantity').html(quantitySlidesModal);
+
+		$('.modal-slider').on('afterChange', function(){
+
+			var countSlidesModal = $('.modal-slider--item.slick-active').index();
+			var modalTitle = $('.modal-slider--item.slick-active').attr('data-title');
+
+			$('.modal-counter .count').html(countSlidesModal);
+			$('.modal-title').html(modalTitle);
+			
+		});
+	}
+
 }
