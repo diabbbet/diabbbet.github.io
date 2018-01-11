b74e6c57b030b9cde6c4ac301dfb27da3d9e3c14
diabbbet <diabbbet@gmail.com>
2018-01-11 04:07:26 +0400

    фы
    фы

--- crowdsale/js/common.js
+++ crowdsale/js/common.js
@@ -154,8 +154,9 @@ $(document).ready(()=>{
   });
 
    particlesJS.load('particles-js', 'js/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+   });
+   particlesJS.load('particles-js-2', 'js/particles.json', function() {
+   });
  });
 
 
