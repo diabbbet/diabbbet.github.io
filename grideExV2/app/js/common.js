e18179fb1b193266629860aaf711068bc8c69a3c
diabbbet <diabbbet@gmail.com>
2018-04-09 02:15:55 +0400

    new update

--- grideExV2/app/js/common.js
+++ grideExV2/app/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
 
-	// Custom JS
+	$('#team_slider').slick({
+		infinite: false,
+		nextArrow: '<button type="button" class="slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-prev"></button>'
+	});
 
 });
