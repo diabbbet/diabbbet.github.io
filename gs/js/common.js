fe6ce2f6bf9e9fc742a5100d80dac51347e70156
diabbbet <diabbbet@gmail.com>
2018-06-24 20:20:39 +0400

    animate

--- gs/js/common.js
+++ gs/js/common.js
@@ -1,34 +1,40 @@
 $(function() {
+	var ii = 0;
 
 	$('#gstudio').removeClass('start');
-	$('.circles-wrapper').addClass('animate');
 	setTimeout(function() {
-		$('.circles-wrapper').removeClass('animate');
-	}, 6000);
-	
+		$('.circles-wrapper').addClass('stop');
+	}, 3000);
 
-	$('#gstudio a').on('click', function() {
-		$('#gstudio').addClass('hide');
-		$('#digital_agency').addClass('show');
-		$('#links').addClass('show');
-		$('.circles-wrapper').removeClass('animate');
-		$('.circles-wrapper').addClass('animate');
-		setTimeout(function() {
-			$('.circles-wrapper').removeClass('animate');
-		}, 6000);
-		return false;
+	$(window).on('wheel', function(e) {
+
+		if (ii == 1) {
+			return false;
+		} else {
+
+			if (event.deltaY > 0) {
+				$('#gstudio').addClass('hide');
+				$('#digital_agency').addClass('show');
+				$('#links').addClass('show');
+				$('.circles-wrapper').removeClass('stop');
+				ii = 1;
+				setTimeout(function() {
+					ii = 0;
+					$('.circles-wrapper').addClass('stop');
+				}, 3000);
+				return false;
+			} else {
+				$('#gstudio').removeClass('hide');
+				$('#digital_agency').removeClass('show');
+				$('#links').removeClass('show');
+				$('.circles-wrapper').removeClass('stop');
+				ii = 1;
+				setTimeout(function() {
+					ii = 0;
+					$('.circles-wrapper').addClass('stop');
+				}, 3000);
+				return false;
+			}
+		}
 	});
-
-	$('#digital_agency a').on('click', function() {
-		$('#gstudio').removeClass('hide');
-		$('#digital_agency').removeClass('show');
-		$('#links').removeClass('show');
-		$('.circles-wrapper').removeClass('animate');
-		$('.circles-wrapper').addClass('animate');
-		setTimeout(function() {
-			$('.circles-wrapper').removeClass('animate');
-		}, 6000);
-		return false;
-	});
-
 });
