e31e4329d2e372d146f29e90626aa676c3e06308
diabbbet <diabbbet@gmail.com>
2018-07-10 23:37:04 +0400

    qwe

--- silk-road/js/common.js
+++ silk-road/js/common.js
@@ -21,7 +21,8 @@ $(function() {
 		var top = $('[data-anchor="'+link+'"]').offset().top;
 		$('body,html').animate({scrollTop: top}, 1000);
 
-		$('.nav.mobile').hide();
+		$('.nav.mobile').removeClass('mobile');
+		$('.burger').removeClass('active');
 		$('body').removeClass('hidden');
 		return false;
 	});
@@ -203,6 +204,8 @@ $(function() {
 		var modal = $(this).attr('data-modal-link');
 		$('[data-modal="'+modal+'"]').show();
 		$('body').addClass('hidden');
+		$('.nav.mobile').removeClass('mobile');
+		$('.burger').removeClass('active');
 		return false;
 	});
 
@@ -344,56 +347,68 @@ function countdown() {
 }
 
 function calculator() {
-	// var month_1 = 0.02286438 * $('#invest').val() * $('#btc').val() / $('#asic').val();
-	// $('#month_1 span').text(Math.round(month_1));
-	// var month_2 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_1 / $('#asic').val() + month_1;
-	// $('#month_2 span').text(Math.round(month_2));
-	// var month_3 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_2 / $('#asic').val() + month_2;
-	// $('#month_3 span').text(Math.round(month_3));
-	// var month_4 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_3 / $('#asic').val() + month_3;
-	// $('#month_4 span').text(Math.round(month_4));
-	// var month_5 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_4 / $('#asic').val() + month_4;
-	// var month_6 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_5 / $('#asic').val() + month_5;
-	// $('#y1 span').text(Math.round(month_6));
-	// var month_7 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_6 / $('#asic').val() + month_6;
-	// var month_8 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_7 / $('#asic').val() + month_7;
-	// var month_9 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_8 / $('#asic').val() + month_8;
-	// var month_10 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_9 / $('#asic').val() + month_9;
-	// var month_11 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_10 / $('#asic').val() + month_10;
-	// var month_12 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_11 / $('#asic').val() + month_11;
-	// var month_13 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_12 / $('#asic').val() + month_12;
-	// var month_14 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_13 / $('#asic').val() + month_13;
-	// var month_15 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_14 / $('#asic').val() + month_14;
-	// var month_16 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_15 / $('#asic').val() + month_15;
-	// var month_17 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_16 / $('#asic').val() + month_16;
-	// var month_18 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_17 / $('#asic').val() + month_17;
-	// $('#y2 span').text(Math.round(month_18));
-	// var  month_19 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_18 / $('#asic').val() + month_18;
-	// var  month_20 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_19 / $('#asic').val() + month_19;
-	// var  month_21 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_20 / $('#asic').val() + month_20;
-	// var  month_22 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_21 / $('#asic').val() + month_21;
-	// var  month_23 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_22 / $('#asic').val() + month_22;
-	// var  month_24 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_23 / $('#asic').val() + month_23;
-	// var  month_25 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_24 / $('#asic').val() + month_24;
-	// var  month_26 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_25 / $('#asic').val() + month_25;
-	// var  month_27 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_26 / $('#asic').val() + month_26;
-	// var  month_28 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_27 / $('#asic').val() + month_27;
-	// var  month_29 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_28 / $('#asic').val() + month_28;
-	// var  month_30 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_29 / $('#asic').val() + month_29;
-	// $('#y3 span').text(Math.round(month_30));
-	// var  month_31 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_30 / $('#asic').val() + month_30;
-	// var  month_32 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_31 / $('#asic').val() + month_31;
-	// var  month_33 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_32 / $('#asic').val() + month_32;
-	// var  month_34 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_33 / $('#asic').val() + month_33;
-	// var  month_35 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_34 / $('#asic').val() + month_34;
-	// var  month_36 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_35 / $('#asic').val() + month_35;
-	// var  month_37 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_36 / $('#asic').val() + month_36;
-	// var  month_38 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_37 / $('#asic').val() + month_37;
-	// var  month_39 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_38 / $('#asic').val() + month_38;
-	// var  month_40 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_39 / $('#asic').val() + month_39;
-	// var  month_41 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_40 / $('#asic').val() + month_40;
-	// var  month_42 = 0.004572876 * $('#invest').val() * $('#btc').val() * month_41 / $('#asic').val() + month_41;
-	// $('#y4 span').text(Math.round(month_42));
+	var month_1 = 0.02286438 * ($('#invest').val() * $('#btc').val() / $('#asic').val());
+	var month_2 = 0.004572876 * ($('#btc').val() * month_1 / $('#asic').val()) + month_1;
+	var month_3 = 0.004572876 * ($('#btc').val() * month_2 / $('#asic').val()) + month_2;
+	var month_4 = 0.004572876 * ($('#btc').val() * month_3 / $('#asic').val()) + month_3;
+	var month_5 = 0.004572876 * ($('#btc').val() * month_4 / $('#asic').val()) + month_4;
+	var month_6 = 0.004572876 * ($('#btc').val() * month_5 / $('#asic').val()) + month_5;
+
+	var y1 = month_1 + month_2 + month_3 + month_4 + month_5 + month_6;
+
+	var month_7 = 0.004572876 * ($('#btc').val() * month_6 / $('#asic').val()) + month_6;
+	var month_8 = 0.004572876 * ($('#btc').val() * month_7 / $('#asic').val()) + month_7;
+	var month_9 = 0.004572876 * ($('#btc').val() * month_8 / $('#asic').val()) + month_8;
+	var month_10 = 0.004572876 * ($('#btc').val() * month_9 / $('#asic').val()) + month_9;
+	var month_11 = 0.004572876 * ($('#btc').val() * month_10 / $('#asic').val()) + month_10;
+	var month_12 = 0.004572876 * ($('#btc').val() * month_11 / $('#asic').val()) + month_11;
+	var month_13 = 0.004572876 * ($('#btc').val() * month_12 / $('#asic').val()) + month_12;
+	var month_14 = 0.004572876 * ($('#btc').val() * month_13 / $('#asic').val()) + month_13;
+	var month_15 = 0.004572876 * ($('#btc').val() * month_14 / $('#asic').val()) + month_14;
+	var month_16 = 0.004572876 * ($('#btc').val() * month_15 / $('#asic').val()) + month_15;
+	var month_17 = 0.004572876 * ($('#btc').val() * month_16 / $('#asic').val()) + month_16;
+	var month_18 = 0.004572876 * ($('#btc').val() * month_17 / $('#asic').val()) + month_17;
+
+	var y2 = month_1 + month_2 + month_3 + month_4 + month_5 + month_6 + month_7 + month_8 + month_9 + month_10 + month_11 + month_12 + month_13 + month_14 + month_15 + month_16 + month_17 + month_18;
+
+	var month_19 = 0.004572876 * ($('#btc').val() * month_18 / $('#asic').val()) + month_18;
+	var month_20 = 0.004572876 * ($('#btc').val() * month_19 / $('#asic').val()) + month_19;
+	var month_21 = 0.004572876 * ($('#btc').val() * month_20 / $('#asic').val()) + month_20;
+	var month_22 = 0.004572876 * ($('#btc').val() * month_21 / $('#asic').val()) + month_21;
+	var month_23 = 0.004572876 * ($('#btc').val() * month_22 / $('#asic').val()) + month_22;
+	var month_24 = 0.004572876 * ($('#btc').val() * month_23 / $('#asic').val()) + month_23;
+	var month_25 = 0.004572876 * ($('#btc').val() * month_24 / $('#asic').val()) + month_24;
+	var month_26 = 0.004572876 * ($('#btc').val() * month_25 / $('#asic').val()) + month_25;
+	var month_27 = 0.004572876 * ($('#btc').val() * month_26 / $('#asic').val()) + month_26;
+	var month_28 = 0.004572876 * ($('#btc').val() * month_27 / $('#asic').val()) + month_27;
+	var month_29 = 0.004572876 * ($('#btc').val() * month_28 / $('#asic').val()) + month_28;
+	var month_30 = 0.004572876 * ($('#btc').val() * month_29 / $('#asic').val()) + month_29;
+
+	var y3 = month_1 + month_2 + month_3 + month_4 + month_5 + month_6 + month_7 + month_8 + month_9 + month_10 + month_11 + month_12 + month_13 + month_14 + month_15 + month_16 + month_17 + month_18 + month_19 + month_20 + month_21 + month_22 + month_23 + month_24 + month_25 + month_26 + month_27 + month_28 + month_29 + month_30;
+
+	var month_31 = 0.004572876 * ($('#btc').val() * month_30 / $('#asic').val()) + month_30;
+	var month_32 = 0.004572876 * ($('#btc').val() * month_31 / $('#asic').val()) + month_31;
+	var month_33 = 0.004572876 * ($('#btc').val() * month_32 / $('#asic').val()) + month_32;
+	var month_34 = 0.004572876 * ($('#btc').val() * month_33 / $('#asic').val()) + month_33;
+	var month_35 = 0.004572876 * ($('#btc').val() * month_34 / $('#asic').val()) + month_34;
+	var month_36 = 0.004572876 * ($('#btc').val() * month_35 / $('#asic').val()) + month_35;
+	var month_37 = 0.004572876 * ($('#btc').val() * month_36 / $('#asic').val()) + month_36;
+	var month_38 = 0.004572876 * ($('#btc').val() * month_37 / $('#asic').val()) + month_37;
+	var month_39 = 0.004572876 * ($('#btc').val() * month_38 / $('#asic').val()) + month_38;
+	var month_40 = 0.004572876 * ($('#btc').val() * month_39 / $('#asic').val()) + month_39;
+	var month_41 = 0.004572876 * ($('#btc').val() * month_40 / $('#asic').val()) + month_40;
+	var month_42 = 0.004572876 * ($('#btc').val() * month_41 / $('#asic').val()) + month_41;
+
+	var y4 = month_1 + month_2 + month_3 + month_4 + month_5 + month_6 + month_7 + month_8 + month_9 + month_10 + month_11 + month_12 + month_13 + month_14 + month_15 + month_16 + month_17 + month_18 + month_19 + month_20 + month_21 + month_22 + month_23 + month_24 + month_25 + month_26 + month_27 + month_28 + month_29 + month_30 + month_31 + month_32 + month_33 + month_34 + month_35 + month_36 + month_37 + month_38 + month_39 + month_40 + month_41 + month_42;
+	
+	$('#month_1 span').text(Math.round(month_1));
+	$('#month_2 span').text(Math.round(month_2));
+	$('#month_3 span').text(Math.round(month_3));
+	$('#month_4 span').text(Math.round(month_4));
+	$('#y1 span').text(Math.round(y1));
+	$('#y2 span').text(Math.round(y2));
+	$('#y3 span').text(Math.round(y3));
+	$('#y4 span').text(Math.round(y4));
 	$(".calculator-graph").addClass('build');
 	setTimeout(function(){
 		$(".calculator-graph").removeClass('build');
