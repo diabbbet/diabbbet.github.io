664286a9eaebcadb49b53f0838b01ce37fe6ba9d
diabbbet <diabbbet@gmail.com>
2018-07-26 02:54:16 +0400

    asd

--- gs/js/common.js
+++ gs/js/common.js
@@ -5,11 +5,14 @@ function ready() {
 	new fullpage('#fullpage', {
 		licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
 		onLeave: function(){
+
 			var begins = document.getElementsByClassName('begin');
 			for (var i = begins.length - 1; i >= 0; i--) {
 				begins[i].beginElement();
 			}
 		}
 	});
+
+	document.getElementById('gstudio_animate').classList.remove('animate');
 
 }
