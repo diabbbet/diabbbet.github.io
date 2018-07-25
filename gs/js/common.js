727a9030cc6a49a108d9df7f5f1c44e27dce4606
diabbbet <diabbbet@gmail.com>
2018-07-26 02:26:24 +0400

    vanilla

--- gs/js/common.js
+++ gs/js/common.js
@@ -1,48 +1,15 @@
-$(function() {
-	var ii = 0;
+document.addEventListener("DOMContentLoaded", ready);
 
-	$('#gstudio').removeClass('start');
-	setTimeout(function() {
-		$('.circles-wrapper').addClass('stop');
-		$('svg animateTransform').attr('repeatCount', '1');
-	}, 3500);
+function ready() {
 
-	$(window).on('mousewheel', function(e) {
-		console.log(e)
-
-		if (ii == 1) {
-			return false;
-		} else {
-
-			if (e.deltaY > 0) {
-				$('#gstudio').addClass('hide');
-				$('#digital_agency').addClass('show');
-				$('#links').addClass('show');
-				$('.circles-wrapper').removeClass('stop');
-				$('svg animateTransform').attr('repeatCount', 'indefinite');
-				
-				ii = 1;
-				setTimeout(function() {
-					ii = 0;
-					$('.circles-wrapper').addClass('stop');
-					$('svg animateTransform').attr('repeatCount', '1');
-				}, 3500);
-				return false;
-			} else {
-				$('#gstudio').removeClass('hide');
-				$('#digital_agency').removeClass('show');
-				$('#links').removeClass('show');
-				$('.circles-wrapper').removeClass('stop');
-				$('svg animateTransform').attr('repeatCount', 'indefinite');
-				
-				ii = 1;
-				setTimeout(function() {
-					ii = 0;
-					$('.circles-wrapper').addClass('stop');
-					$('svg animateTransform').attr('repeatCount', '1');
-				}, 3500);
-				return false;
+	new fullpage('#fullpage', {
+		licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+		onLeave: function(){
+			var begins = document.getElementsByClassName('begin');
+			for (var i = begins.length - 1; i >= 0; i--) {
+				begins[i].beginElement();
 			}
 		}
 	});
-});
+
+}
